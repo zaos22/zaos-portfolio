7695476244164dc26d86c26e95a03a66f0a8c8bf
ZaynOssama <oussamazazou@iesmontsia.org>
2023-11-22 20:18:45 +0100

    Changed the route of my CV

--- src/Me/Me_light.js
+++ src/Me/Me_light.js
@@ -9,7 +9,7 @@ function Me() {
 
     const handleDownloadCV = () => {
         // Replace 'path/to/your/cv.pdf' with the actual path to your PDF file
-        const pdfPath = 'path/to/your/cv.pdf';
+        const pdfPath = './My_CV.pdf';
         window.open(pdfPath, '_blank');
     };
 
